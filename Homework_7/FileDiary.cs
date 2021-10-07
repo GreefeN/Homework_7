590131a1db87770d6368fd1f46a9abc926d63dfa
GreefeN <km.abramov@gmail.com>
2021-10-07 22:47:50 +0300

    complete old homework7

---
+++ Homework_7/FileDiary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Homework_7
+{
+    /// <summary>
+    /// структура для работы с файлом ежедневника
+    /// </summary>
+    struct FileDiary
+    {
+        public Diary diary;
+        private string path;
+
+        /// <summary>
+        /// конструктор для чтения файла ежедневника или его создания
+        /// </summary>
+        /// <param name="path"></param>
+        public FileDiary(string path)
+        {
+            using (StreamReader sr = new StreamReader(File.Open(path, FileMode.OpenOrCreate)))
+            {                
+                string line;
+                List<Unit> Note = new List<Unit>();
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] arr = line.Split('#');
+                    Note.Add(new Unit(arr[0], arr[1], int.Parse(arr[2]), double.Parse(arr[3]), Convert.ToDateTime(arr[4])));
+                }
+                diary = new Diary(Note);
+                this.path = path;
+            }
+        }
+
+        /// <summary>
+        /// метод для записи в файл ежедневника
+        /// </summary>
+        public void WriteDiary()
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                foreach (Unit u in diary.Note)
+                {
+                    sw.WriteLine(u.getUnitString());
+                }
+            }
+        }
+    }
+}
---
+++ Homework_7/Unit.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Homework_7
+{
+    /// <summary>
+    /// структура описывающая запись ежедневника
+    /// </summary>
+    struct Unit
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public DateTime WritingDate { get; set; } //время записи/изменения
+        public double Score { get; set; } //количество баллов
+
+        /// <summary>
+        /// конструктор для записей из файла
+        /// </summary>
+        /// <param name="ln"></param>
+        /// <param name="fn"></param>
+        /// <param name="Age"></param>
+        /// <param name="s"></param>
+        /// <param name="dt"></param>
+        public Unit(string ln, string fn, int Age, double s, DateTime dt)
+        {
+            FirstName = fn;
+            LastName = ln;
+            this.Age = Age;
+            WritingDate = dt;
+            Score = s;
+        }
+
+        /// <summary>
+        /// конструктор для создаваемых записей
+        /// </summary>
+        /// <param name="ln"></param>
+        /// <param name="fn"></param>
+        /// <param name="Age"></param>
+        /// <param name="s"></param>
+        public Unit(string ln, string fn, int Age, double s) : this(ln, fn, Age, s, DateTime.Today)
+        {
+        }
+
+        /// <summary>
+        /// индексатор для изменения отдельных свойств структуры с авто изменением даты
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public string this[string s]
+        {
+            set
+            {
+                switch (s)
+                {
+                    case "и":
+                        FirstName = value;
+                        WritingDate = DateTime.Today;
+                        break;
+                    case "ф":
+                        LastName = value;
+                        WritingDate = DateTime.Today;
+                        break;
+                    case "в":
+                        Age = Convert.ToInt32(value);
+                        WritingDate = DateTime.Today;
+                        break;
+                    case "б":
+                        Score = Convert.ToDouble(value);
+                        WritingDate = DateTime.Today;
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// выводит информацию о записи, добавляя разделение строк вывода
+        /// </summary>
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{LastName,10}\t{FirstName,10}\t{Age,3}\t{Score,17}\t{WritingDate.ToString("d")}");
+            Console.WriteLine(new string('-', Console.WindowWidth));
+        }
+
+        /// <summary>
+        /// возвращает строку содержащую свойства записи
+        /// </summary>
+        /// <returns></returns>
+        public string getUnitString()
+        {
+            return $"{LastName}#{FirstName}#{Age}#{Score}#{WritingDate.ToString("d")}";
+        }
+    }
+}
